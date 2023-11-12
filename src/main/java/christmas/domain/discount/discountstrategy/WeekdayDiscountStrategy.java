b4e2d77803ab7f05146aeeafc9ec0bbe682724d8
song windows <dkclasltmf@naver.com>
2023-11-12 23:54:06 +0900

    fix: 요일 할인 혜택 검증 로직 수정

--- src/main/java/christmas/domain/discount/discountstrategy/WeekdayDiscountStrategy.java
+++ src/main/java/christmas/domain/discount/discountstrategy/WeekdayDiscountStrategy.java
@@ -11,23 +11,26 @@ import christmas.utils.WeekdayPredicate;
 
 public class WeekdayDiscountStrategy implements DiscountStrategy {
     private final ReservationDate reservationDate;
+    private final WeekPredicate weekPredicate;
+
 
     public WeekdayDiscountStrategy(ReservationDate reservationDate) {
         this.reservationDate = reservationDate;
+        this.weekPredicate = new WeekdayPredicate();
     }
 
     @Override
     public DiscountBenefit calculateDiscount(Order order) {
-        WeekPredicate weekPredicate = new WeekdayPredicate();
-        return createDiscountBenefit(order, weekPredicate);
+        Integer weekDiscountMoney = order.getWeekDiscountMoney(MenuCategory.DESSERT);
+
+        return createDiscountBenefit(weekDiscountMoney, weekPredicate);
     }
 
-    private DiscountBenefit createDiscountBenefit(Order order, WeekPredicate weekPredicate) {
-        if (!weekPredicate.match(reservationDate)) {
+    private DiscountBenefit createDiscountBenefit(Integer weekDiscountMoney, WeekPredicate weekPredicate) {
+        if (!weekPredicate.match(reservationDate) || weekDiscountMoney == 0) {
             return new NoDiscountBenefit();
         }
 
-        Integer weekDiscountMoney = order.getWeekDiscountMoney(MenuCategory.DESSERT);
         return new WeekDiscountBenefit(weekPredicate.getDescription(), weekDiscountMoney);
     }
 }
