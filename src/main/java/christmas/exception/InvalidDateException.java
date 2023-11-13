6d57ca678ef537a9c8170eb3d1dc56addedf7458
song windows <dkclasltmf@naver.com>
2023-11-13 18:15:35 +0900

    refactor: 예외 메시지를 상수로 추출 및 접근제어자 수정

--- src/main/java/christmas/exception/InvalidDateException.java
+++ src/main/java/christmas/exception/InvalidDateException.java
@@ -1,8 +1,7 @@
 package christmas.exception;
 
 public class InvalidDateException extends IllegalArgumentException {
-
-    public static final String MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     public InvalidDateException() {
         super(MESSAGE);
--- src/main/java/christmas/exception/InvalidInputException.java
+++ src/main/java/christmas/exception/InvalidInputException.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public class InvalidInputException extends IllegalArgumentException {
-    public static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private static final String MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     public InvalidInputException() {
         super(MESSAGE);
