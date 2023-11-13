af169cf10d61dd6f627f46fae3ba1930e5c5718c
song windows <dkclasltmf@naver.com>
2023-11-13 15:24:53 +0900

    feat: GameConfig 생성
    게임 클래스들을 한곳에서 관리

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,22 +1,12 @@
 package christmas;
 
 import christmas.controller.GameController;
-import christmas.input.handler.InputHandler;
-import christmas.output.handler.OutputHandler;
-import christmas.domain.order.OrderService;
-import christmas.input.convertor.InputConvertor;
-import christmas.input.validator.InputValidator;
-import christmas.input.view.InputView;
-import christmas.output.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameController gameController = new GameController(
-                new InputHandler(
-                        new InputView(), new InputConvertor(new InputValidator())),
-                new OutputHandler(
-                        new OutputView()), new OrderService());
+        GameConfig gameConfig = new GameConfig();
+        GameController gameController = gameConfig.gameController();
 
         gameController.startGame();
     }
